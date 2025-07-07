eaea19bb53bf6425bf379f44aee79c2b8a7b2d2a
Norix <121669280+Norix10@users.noreply.github.com>
2025-07-07 15:44:36 +0300

    create function for automatic table names

--- core/models/base.py
+++ core/models/base.py
@@ -1,5 +1,10 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
 class Base(DeclarativeBase):
     abstract = True
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+
     id: Mapped[int] = mapped_column(primary_key=True)
