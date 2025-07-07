976155620f28b805dffbbeaf3d813a0e62ec4eed
Norix <121669280+Norix10@users.noreply.github.com>
2025-07-07 13:35:09 +0300

    create migrations for products table

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from api_v1 import router as router_v1
 from items_views import router as items_router
 from users.views import router as user_router
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with db_healper.engine.begin() as conn:
@@ -20,6 +21,7 @@ app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 app.include_router(items_router)
 app.include_router(user_router)
 
+
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"} 
+    return {"Hello": "World"}
