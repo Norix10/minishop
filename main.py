4733cbdedd8ec1bebc4176827090c8a2489af642
Norix <121669280+Norix10@users.noreply.github.com>
2025-07-07 13:38:13 +0300

    no more create all on app start

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 
 from core.config import settings
-from core.models import Base, db_healper
 from api_v1 import router as router_v1
 from items_views import router as items_router
 from users.views import router as user_router
@@ -11,8 +10,6 @@ from users.views import router as user_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_healper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
